6b796b3bad482010c279d90382e7bd8212dde83c
Restyled.io <commits@restyled.io>
2023-02-13 21:26:59 +0000

    Restyled by clang-format

--- UdpTest-main/UdpTest-main/server.cpp
+++ UdpTest-main/UdpTest-main/server.cpp
@@ -4,34 +4,33 @@
 
 using namespace std;
 
-const char* PORT = "34369";
-const char* RECV_PORT = "34370";
-const char* CLIENT = "10.65.65.87"; // my IP address given by huky on net
+const char *PORT = "34369";
+const char *RECV_PORT = "34370";
+const char *CLIENT = "10.65.65.87"; // my IP address given by huky on net
 
 const int MSG_SIZE = 2000;
 
-int main()
-{
-    UdpSocket udpSock(PORT);
-    UdpSocket reciever(RECV_PORT);
-    reciever.setDestAddress(CLIENT);
+int main() {
+  UdpSocket udpSock(PORT);
+  UdpSocket reciever(RECV_PORT);
+  reciever.setDestAddress(CLIENT);
 
-    int msg[1];
+  int msg[1];
 
-    // udpSock.recvFrom((char*)msg, 4);
-    // reciever.setSrcAddr(udpSock.getSrcAddr());
-    // if (reciever.sendTo((char*)msg, 4) < 0) {
-    //    cerr << "error sending msg";
-    // }
-    // cout << msg[0] << endl;
+  // udpSock.recvFrom((char*)msg, 4);
+  // reciever.setSrcAddr(udpSock.getSrcAddr());
+  // if (reciever.sendTo((char*)msg, 4) < 0) {
+  //    cerr << "error sending msg";
+  // }
+  // cout << msg[0] << endl;
 
-    while (true) {
-        while (udpSock.pollRecvFrom()) {
-            udpSock.recvFrom((char*)msg, 4);
-            if (reciever.sendTo((char*)msg, 4) < 0) {
-                cerr << "error sending msg" << endl;
-            }
-            cout << msg[0] << endl;
-        }
+  while (true) {
+    while (udpSock.pollRecvFrom()) {
+      udpSock.recvFrom((char *)msg, 4);
+      if (reciever.sendTo((char *)msg, 4) < 0) {
+        cerr << "error sending msg" << endl;
+      }
+      cout << msg[0] << endl;
     }
+  }
 }
