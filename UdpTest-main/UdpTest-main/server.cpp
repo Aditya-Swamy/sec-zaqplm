f64c683c5d251d9a6e266275f0c5745cae7dc152
Aditya Siddalingaswamy <37868635+Aditya-Swamy@users.noreply.github.com>
2023-02-13 23:22:10 -0800

    Added Josh's UDP test code (#1)
    * Added Josh's UDP test code
    
    To verify github security actions and test workflows
    
    * Restyle Added Josh's UDP test code (#2)
    
    * Restyled by astyle
    
    * Restyled by clang-format
    
    * Restyled by prettier-markdown
    
    * Restyled by whitespace
    
    ---------
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    ---------
    
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

---
+++ UdpTest-main/UdpTest-main/server.cpp
@@ -0,0 +1,36 @@
+#include "Timer.h"
+#include "UdpSocket.h"
+#include <ostream>
+
+using namespace std;
+
+const char *PORT = "34369";
+const char *RECV_PORT = "34370";
+const char *CLIENT = "10.65.65.87"; // my IP address given by huky on net
+
+const int MSG_SIZE = 2000;
+
+int main() {
+  UdpSocket udpSock(PORT);
+  UdpSocket reciever(RECV_PORT);
+  reciever.setDestAddress(CLIENT);
+
+  int msg[1];
+
+  // udpSock.recvFrom((char*)msg, 4);
+  // reciever.setSrcAddr(udpSock.getSrcAddr());
+  // if (reciever.sendTo((char*)msg, 4) < 0) {
+  //    cerr << "error sending msg";
+  // }
+  // cout << msg[0] << endl;
+
+  while (true) {
+    while (udpSock.pollRecvFrom()) {
+      udpSock.recvFrom((char *)msg, 4);
+      if (reciever.sendTo((char *)msg, 4) < 0) {
+        cerr << "error sending msg" << endl;
+      }
+      cout << msg[0] << endl;
+    }
+  }
+}
